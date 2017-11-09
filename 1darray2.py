d32fbc44c2a787051e8c15b43f4b477cb644bb44
familysimpson <thefamilysimpson@gmail.com>
2017-11-09 15:22:24 +0000

    Nov assessment walkthrough

--- 1darray2.py
+++ 1darray2.py
@@ -1,29 +1,33 @@
 # DECLARE ages : ARRAY[0..9] OF INTEGER
 # DECLARE temp : INTEGER
 #ages = [43,65,34,63,13,78,64,26,9,4]
-ages = []
-for index in range(0,10):
+
+def output_array_contents():
+    # DECLARE index : INTEGER
+    fh = open("num.txt","w")
+    for index in range(0,10):
+        fh.write(str(ages[index]))
+        #print(ages[index])
+    fh.close()
+
+def bubble_sort():
+    for passes in range(0, 9):
+        for index in range(0, 9):
+            if ages[index] > ages[index + 1]:
+                temp = ages[index]
+                ages[index] = ages[index + 1]
+                ages[index + 1] = temp
+
+def get_valid_age(): # RETURNS integer
     temp = int(input("Enter age (0-99)"))
     while temp < 0 or temp > 99:
         print("Error.")
         temp = int(input("Enter age (0-99)"))
-    ages.append(temp)
+    return temp
 
+ages = []
 for index in range(0,10):
-    print(ages[index])
-
-for passes in range(0, 9):
-    for index in range(0, 9):
-        if ages[index] > ages[index+1]:
-            temp = ages[index]
-            ages[index] = ages[index+1]
-            ages[index+1] = temp
-
-for index in range(0, 10):
-    print(ages[index])
-
-
-
-
-
-                #ages.append(45)
+    ages.append(get_valid_age())
+output_array_contents()
+bubble_sort()
+output_array_contents()
