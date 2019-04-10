0ba9ac2c163e3846aaeb336577a7472c12ef3bb2
Ian Simpson <mrsimpson.rgc@gmail.com>
2019-04-10 21:50:57 +0200

    Update A2_weekly_binarysearch.py
    Used algorithm discussed in tutorial 10. See comments for detail.

--- NC homework/A2_weekly_binarysearch.py
+++ NC homework/A2_weekly_binarysearch.py
@@ -1,28 +1,23 @@
 # DECLARE entry, middle, top, bottom : INTEGER
 # DECLARE testscore : ARRAY[0:6] OF INTEGER
 testscore = [5,12,24,43,48,58,76]
-entry = int(input("Enter ")) # changed input.. to int(input..)
-n = len(testscore)
-middle = round(n/2)
+entry = int(input("Enter ")) # IS changed input.. to int(input..)
+n = len(testscore) -1 # IS added -1 to ensure index out of range error did not occur
 top = n
 bottom = 0
-if entry == testscore[middle]:
-    print("Value at", middle)
-elif entry > testscore[middle]:
-    bottom = middle + 1
-    middle = (bottom + top) / 2
-    if entry == testscore[bottom]: # changed elif to IF
-        print(bottom)
-    elif entry == testscore[top]:
-        print(top)
-    else:
-        print("not in list")
-elif entry < testscore[middle]:
-    top = middle - 1
-    middle = (top + bottom) / 2
-    if entry == testscore[top]:
-        print(top)
-    elif entry == testscore[bottom]:
-        print(bottom)
-    else:
-        print("Not in list")
+index = -1 # IS added
+
+while (index == -1) and (bottom <= top): # IS added
+    middle = (bottom + top) // 2 # IS changed divide / to floor //
+    #print(bottom,top,middle,index,entry) # uncomment this line to see program trace
+    if entry == testscore[middle]:
+        index = middle # IS added
+    elif entry < testscore[middle]:
+        top = middle - 1
+    else: # IS added
+        bottom = middle + 1
+    
+if index != -1: # IS added
+    print("Value at", index) # IS changed middle to index
+else: # IS added
+    print("Not in list")
